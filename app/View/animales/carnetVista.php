7ed58e7dd8e38729eec4ba92471eed21930b6d51
Diego Ortega Salas <dialex1205@gmail.com>
2024-08-29 03:16:34 -0600

    filtros por usuario pare ver carnets
    Ahora el creador de los carnets ve sus carnets de primero además de las opciones de eliminar y actualizar, cosa que no ve con otros que no fueron creados por él.

--- app/View/animales/carnetVista.php
+++ app/View/animales/carnetVista.php
@@ -1,5 +1,6 @@
-<?php include_once '../layout.php';
-include_once '../../Controller/CarnetController.php';
+<?php
+include_once '../layout.php';
+include_once '../../Controller/carnetController.php';
 
 $carnets = carnetController::obtenerCarnets();
 ?>
@@ -24,28 +25,38 @@ head();
                 <div class="container">
                     <div class="p-5">
                         <div class="text-center mb-5">
-                            <h1>Lista de Animales</h1>
-                        </div>
-                        <div class="row">
-                            <?php foreach ($carnets as $carnet): ?>
-                                <div class="col-md-4">
-                                    <div class="card" style="width: 18rem;">
-                                        <?php if ($carnet['IMAGEN']): ?>
-                                            <img src="/G3_SC502_VN_Proyecto/app/View/subirImg/<?php echo basename($carnet['IMAGEN']); ?>" class="card-img-top" alt="<?php echo htmlspecialchars($carnet['NOMBRE_ANIMAL']); ?>">
-                                        <?php endif; ?>
-                                        <div class="card-body">
-                                            <h5 class="card-title"><?php echo htmlspecialchars($carnet['NOMBRE_ANIMAL']); ?></h5>
-                                            <p class="card-text">Raza: <?php echo htmlspecialchars($carnet['RAZA']); ?></p>
-                                            <p class="card-text">Fecha de Rescate: <?php echo htmlspecialchars(date('d-m-Y', strtotime($carnet['FECHA_RESCATE']))); ?></p>
-                                            <a href="#" class="btn btn-primary btn-sm">Actualizar</a>
-                                            <a href="#" class="btn btn-danger btn-sm">Eliminar</a>
-                                            <a href="#" class="btn btn-info btn-sm">Adoptar</a>
+                            <h1>Encontrar a tu mascota</h1>
+                            <div class="row">
+                                <?php
+                                usort($carnets, function ($a, $b) {
+                                    if ($a['ID_USUARIO'] == $_SESSION['id_usuario']) return -1;
+                                    if ($b['ID_USUARIO'] == $_SESSION['id_usuario']) return 1;
+                                    return 0;
+                                });
+
+                                foreach ($carnets as $carnet): ?>
+                                    <div class="col-md-4">
+                                        <div class="card" style="width: 18rem;">
+                                            <?php if ($carnet['IMAGEN']): ?>
+                                                <img src="/G3_SC502_VN_Proyecto/app/View/subirImg/<?php echo basename($carnet['IMAGEN']); ?>" class="card-img-top" alt="<?php echo htmlspecialchars($carnet['NOMBRE_ANIMAL']); ?>" style="width: 100%; height: 200px; object-fit: cover;">
+                                            <?php endif; ?>
+                                            <div class="card-body">
+                                                <h5 class="card-title"><?php echo htmlspecialchars($carnet['NOMBRE_ANIMAL']); ?></h5>
+                                                <p class="card-text">Raza: <?php echo htmlspecialchars($carnet['RAZA']); ?></p>
+                                                <p class="card-text">Fecha de Rescate: <?php echo htmlspecialchars(date('d-m-Y', strtotime($carnet['FECHA_RESCATE']))); ?></p>
+
+                                                <?php if ($carnet['ID_USUARIO'] == $_SESSION['id_usuario'] || (isset($_SESSION['ID_ROL']) && $_SESSION['ID_ROL'] == 1)): ?>
+                                                    <a href="#" class="btn btn-primary btn-sm">Actualizar</a>
+                                                    <a href="#" class="btn btn-danger btn-sm">Eliminar</a>
+                                                <?php endif; ?>
+
+                                                <a href="#" class="btn btn-info btn-sm">Adoptar</a>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            <?php endforeach; ?>
+                                <?php endforeach; ?>
+                            </div>
                         </div>
-                    </div>
             </section>
         </div>
 
